cda577fb298faf608a99b2f5ea1fd24e336e34dd
Shubhankar Saxena <saxena03121999@gmail.com>
2020-10-06 22:05:42 +0530

    Updates

---
+++ precache-manifest.647c195184c876f98b2210bcd4e7f95b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "529a31fed34afbc7e1389644284185d2",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "fe4b5b4390fb5b8e42e0",
+    "url": "/Covid-19-Tracker/static/css/main.d5d226a7.chunk.css"
+  },
+  {
+    "revision": "91aefc75da74fea96917",
+    "url": "/Covid-19-Tracker/static/js/2.f506ea43.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/Covid-19-Tracker/static/js/2.f506ea43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe4b5b4390fb5b8e42e0",
+    "url": "/Covid-19-Tracker/static/js/main.a8c5c0b9.chunk.js"
+  },
+  {
+    "revision": "ab69c196ed6b8282a8b3",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.f4b2508f.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19-Tracker/static/media/image.d7265326.png"
+  }
+]);
